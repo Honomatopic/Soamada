5e7d2ee2331be3ea9b9978ba155c3a7b2322cf20
honore <honore.rasamoelina@gmail.com>
2019-04-19 22:02:14 +0200

    Refactoring code de la méthode editerArticlesAction()

--- src/AssocBundle/Controller/AssocController.php
+++ src/AssocBundle/Controller/AssocController.php
@@ -7,16 +7,16 @@ use AssocBundle\Entity\Article;
 use AssocBundle\Entity\Message;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class AssocController extends Controller
-{
+class AssocController extends Controller {
+
     /**
      * Méthode permettant d'afficher la page d'accueil
      *
      * @return void
      */
-    public function indexAction()
-    {
+    public function indexAction() {
         return $this->render('AssocBundle:Default:index.html.twig');
     }
 
@@ -26,8 +26,7 @@ class AssocController extends Controller
      * @param Request $request
      * @return void
      */
-    public function envoiAction(Request $request)
-    {
+    public function envoiAction(Request $request) {
         $message = new Message();
         $form = $this->createForm('AssocBundle\Form\MessageType', $message);
         $form->handleRequest($request);
@@ -38,28 +37,27 @@ class AssocController extends Controller
             $message->getId();
             //$message->setEmail($form["email"]->getData());
             $request->getSession()->getFlashBag()->add('info', 'Message bien envoyé, on vous répondra dans les plus brefs délais');
-     
-             $message = $this->getDoctrine()->getRepository('AssocBundle:Message')->findById($message->getId());
-             $contact = \Swift_Message::newInstance()
-                ->setSubject('Un nouveau message sur le formulaire de contact')
-                ->setFrom($form["email"]->getData())
-                ->setTo('honore.rasamoelina@gmail.com')
-                ->setBody(
+
+            $message = $this->getDoctrine()->getRepository('AssocBundle:Message')->findById($message->getId());
+            $contact = \Swift_Message::newInstance()
+                    ->setSubject('Un nouveau message sur le formulaire de contact')
+                    ->setFrom($form["email"]->getData())
+                    ->setTo('honore.rasamoelina@gmail.com')
+                    ->setBody(
                     $this->renderView(
-                        'Emails/contact.html.twig', array('messages' => $message)
-                    ),
-                    'text/html'
-                );
+                            'Emails/contact.html.twig', array('messages' => $message)
+                    ), 'text/html'
+            );
             $this->get('mailer')->send($contact);
-         }
-            
+        }
+
 
         $membres = $this->getDoctrine()->getRepository('AppBundle:Membre')->findAll();
         // Puis on retourne la vue pour qu'elle affiche la page d'accueil
         return $this->render('AssocBundle:Default:index.html.twig', array('form' => $form->createView(),
-            //'post' => $_POST,
-            'message' => $message,
-            'membres' => $membres,
+                    //'post' => $_POST,
+                    'message' => $message,
+                    'membres' => $membres,
         ));
     }
 
@@ -68,8 +66,7 @@ class AssocController extends Controller
      *
      * @return void
      */
-    public function afficherJournalAction()
-    {
+    public function afficherJournalAction() {
         $articles = $this->getDoctrine()->getRepository('AssocBundle:Article')->findAll();
 
         return $this->render('AssocBundle:Default:journal.html.twig', array('articles' => $articles));
@@ -81,17 +78,23 @@ class AssocController extends Controller
      * @param Request $request
      * @return void
      */
-    public function creerArticlesAction(Request $request)
-    {
+    public function creerArticlesAction(Request $request) {
         $article = new Article();
         $form = $this->createForm('AssocBundle\Form\ArticleType', $article);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid() && $request->isMethod('POST')) {
+            //$photo = $article->getPhoto();
+            //$nomphoto = md5(uniqid()) . '.' . $photo->guessExtension();
+            //$photo->move(
+                    //$this->getParameter('photos_directory'), $nomphoto
+            //);
+            //$article->setPhoto($nomphoto);
+            //$this->generateUrl('assoc_journal');
             $em = $this->getDoctrine()->getManager();
             $em->persist($article);
             $em->flush($article);
             $request->getSession()->getFlashBag()->add('success', 'Article crée !');
-            return $this->render('AssocBundle:Default:creerarticles.html.twig', array('form' => $form->createView()));
+           return $this->redirectToRoute('assoc_journal');
         }
         return $this->render('AssocBundle:Default:creerarticles.html.twig', array('form' => $form->createView(), 'article' => $article));
     }
@@ -104,32 +107,20 @@ class AssocController extends Controller
      * @param Article $id
      * @return void
      */
-    public function editerArticlesAction(Request $request, Article $articles, Article $id)
-    {
-        $articles = $this->getDoctrine()->getRepository('AssocBundle:Article')->find($id);
-        $articles->setTitre($articles->getTitre());
-        $articles->setAuteur($articles->getAuteur());
-        $articles->setCorps($articles->getCorps());
-        $articles->setDate($articles->getDate());
-        $form = $this->createForm('AssocBundle\Form\ArticleType', $articles);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid() && $request->isMethod('POST')) {
-            $em = $this->getDoctrine()->getManager();
-            $titre = $form["titre"]->getData();
-            $auteur = $form["auteur"]->getData();
-            $corps = $form["corps"]->getData();
-            $date = $form["date"]->getData();
-            $articles->setTitre($titre);
-            $articles->setAuteur($auteur);
-            $articles->setCorps($corps);
-            $articles->setDate($date);
-            $em->persist($articles);
-            $em->flush($articles);
-            $request->getSession()->getFlashBag()->add('success', 'Article modifié !');
-
-            return $this->redirectToRoute('assoc_journal');
+    public function editerArticlesAction(Request $request, Article $article, Article $id) {
+        $em = $this->getDoctrine()->getManager();
+        $article = $em->getRepository('AssocBundle:Article')->find($id);
+        if (null == $article) {
+            throw new \Http\Discovery\Exception\NotFoundException("L'article avec l'id ".$id. "n'existe pas");
         }
-        return $this->render('AssocBundle:Default:editerarticles.html.twig', array('form' => $form->createView(), 'article' => $articles));
+        $form = $this->createForm('AssocBundle\Form\ArticleType', $article);
+        $form->handleRequest($request);
+        if ($form->isValid() && $request->isMethod('POST')) {
+            $em->flush($article);
+            $request->getSession()->getFlashBag()->add('success', 'Article modifié !');
+            return $this->redirectToRoute('assoc_journal', array('id' => $article->getId()));
+        }
+        return $this->render('AssocBundle:Default:editerarticles.html.twig', array('form' => $form->createView(), 'article' => $article));
     }
 
     /**
@@ -139,8 +130,7 @@ class AssocController extends Controller
      * @param Article $articles
      * @return void
      */
-    public function supprimerArticlesAction(Request $request, Article $articles)
-    {
+    public function supprimerArticlesAction(Request $request, Article $articles) {
         $em = $this->getDoctrine()->getManager();
         $em->remove($articles);
         $em->flush();
@@ -153,8 +143,7 @@ class AssocController extends Controller
      *
      * @return void
      */
-    public function inscriptionNewsletterAction(Request $request)
-    {
+    public function inscriptionNewsletterAction(Request $request) {
         $abonnes = new Abonne();
 
         $form = $this->createForm('AssocBundle\Form\AbonneType', $abonnes);
@@ -165,15 +154,14 @@ class AssocController extends Controller
             $em->flush($abonnes);
             $request->getSession()->getFlashBag()->add('success', 'Inscription à la newsletter réussie');
             $lettreinfo = \Swift_Message::newInstance()
-                ->setSubject('La lettre d\'information')
-                ->setFrom('admin@admin.com')
-                ->setTo($abonnes->getEmailabonne())
-                ->setBody(
+                    ->setSubject('La lettre d\'information')
+                    ->setFrom('admin@admin.com')
+                    ->setTo($abonnes->getEmailabonne())
+                    ->setBody(
                     $this->renderView(
-                        'Emails/inscription.html.twig'
-                    ),
-                    'text/html'
-                );
+                            'Emails/inscription.html.twig'
+                    ), 'text/html'
+            );
             $this->get('mailer')->send($lettreinfo);
             return $this->render('AssocBundle:Default:inscriptionnews.html.twig', array('form' => $form->createView()));
         }
@@ -185,8 +173,7 @@ class AssocController extends Controller
      *
      * @return void
      */
-    public function listeAbonnesAction()
-    {
+    public function listeAbonnesAction() {
         $abonnes = $this->getDoctrine()->getRepository('AssocBundle:Abonne')->findAll();
         return $this->render('AssocBundle:Default:listeabonnenews.html.twig', array('abonnes' => $abonnes));
     }
@@ -197,23 +184,23 @@ class AssocController extends Controller
      * @param Request $request
      * @return void
      */
-    public function envoyerNewsletterAction(Request $request, $parameters = array())
-    {
+    public function envoyerNewsletterAction(Request $request, $parameters = array()) {
         $abonnes = $this->getDoctrine()->getRepository('AssocBundle:Abonne')->findAll();
         $articles = $this->getDoctrine()->getRepository('AssocBundle:Article')->findAll();
         foreach ($abonnes as $abonne) {
 
             $lettreinfo = \Swift_Message::newInstance()
-                ->setSubject('La lettre d\'information')
-                ->setFrom('admin@admin.com')
-                ->setTo($abonne->getEmailabonne())
-                ->setContentType("text/html")
-                ->setBody($this->renderView(
-                    'Emails/lettreinfos.html.twig', array('articles' => $articles)), 'text/html'  
-                );
+                    ->setSubject('La lettre d\'information')
+                    ->setFrom('admin@admin.com')
+                    ->setTo($abonne->getEmailabonne())
+                    ->setContentType("text/html")
+                    ->setBody($this->renderView(
+                            'Emails/lettreinfos.html.twig', array('articles' => $articles)), 'text/html'
+            );
         }
         $this->get('mailer')->send($lettreinfo);
         $request->getSession()->getFlashBag()->add('success', 'Envoi des news réussie');
         return $this->render('Emails/lettreinfos.html.twig', array('articles' => $articles, 'abonnes' => $abonnes));
     }
+
 }
--- src/AssocBundle/Form/ArticleType.php
+++ src/AssocBundle/Form/ArticleType.php
@@ -8,30 +8,33 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class ArticleType extends AbstractType
-{
+class ArticleType extends AbstractType {
+
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('titre', TextType::class, array('attr' => array('placeholder' => 'Le titre de l\'article', 'class' => 'form-control')))
-            ->add('auteur', TextType::class, array('attr' => array('placeholder' => 'L\'auteur de l\'article', 'class' => 'form-control')))
-            ->add('corps', TextareaType::class, array('attr' => array('placeholder' => 'Le corps de l\'article', 'class' => 'form-control', 'rows'=>'5')))
-            ->add('date', DateType::class, array('attr' => array('placeholder' => 'La date de l\'article', 'class' => 'form-control')))
+                ->add('titre', TextType::class)
+                ->add('auteur', TextType::class)
+                ->add('corps', TextareaType::class)
+                ->add('date', DateType::class, array('widget' => 'single_text',
+                    'format' => 'dd-MM-yyyy',))
+        //->add('photo', FileType::class, array('attr' =>array('placeholder' => 'La photo de l\'article', 'class'=>'form-control')))
         ;
     }
 
     /**
      * @param OptionsResolver $resolver
      */
-    public function configureOptions(OptionsResolver $resolver)
-    {
+    public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'AssocBundle\Entity\Article',
         ));
     }
+
 }
